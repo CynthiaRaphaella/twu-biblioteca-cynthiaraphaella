16d523ec356db5bb375fb1a84ed88e06622336c1
CynthiaRaphaella <cynthia_raphaella02@hotmail.com>
2015-07-24 11:07:23 -0300

    List all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,12 +1,27 @@
 package com.twu.biblioteca;
 
+import com.twu.biblioteca.data.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+    public List<Book> books = new ArrayList<Book>();
 
     public String startBibliotecaApplication(){
         return "Bem-vindo ao sistema de biblioteca";
+    }
+
+    public void addBookToBiblioteca(Book book){
+        books.add(book);
+    }
+
+    public List<Book> showAllBooks(){
+        return books;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
     }
 }
--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,7 +1,11 @@
 package com.twu.biblioteca;
 
 
+import com.twu.biblioteca.data.Book;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -17,5 +21,16 @@ public class BibliotecaTest {
         BibliotecaApp app = new BibliotecaApp();
         String message = app.startBibliotecaApplication();
         assertNotNull(message);
+    }
+
+    @Test
+    public void listAllBooksWhenStart(){
+        BibliotecaApp app = new BibliotecaApp();
+        Book book1 = new Book("Book 1");
+        app.addBookToBiblioteca(book1);
+
+        List<Book> bookList = app.showAllBooks();
+        assertEquals("Book 1", bookList.get(0).name);
+
     }
 }
